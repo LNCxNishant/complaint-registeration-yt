af33fb9f87e263aae09f4ecd06478c20e5479677
LNCxNishant <114398745+LNCxNishant@users.noreply.github.com>
2024-03-07 15:24:31 +0530

    Check for contract connection

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,17 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
+  const clientID = '466a7ec885c491bf25d06b4471c97787';
+
   return (
-    <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <ThirdwebProvider desiredChainId={ChainId.Mumbai} clientId={clientID}>
+      {/* Rendering the main component */}
       <Component {...pageProps} />
+      {/* Adding a toaster component */}
       <Toaster />
     </ThirdwebProvider>
-  )
+  );
 }
 
-export default MyApp
+// Exporting the app as the default export
+export default MyApp;
